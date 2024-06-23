ddb44a960841f91f0255eec0c87e2a3d14a1673c
Abhishek <abhitiwari3010@gmail.com>
2024-06-23 08:21:51 +0530

    changed the image

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -5,7 +5,7 @@ const Intro =  () => {
     <div className="bg-white p-4 rounded-lg shadow-md ml-9 mr-9">
     <div className="text-xl font-bold mb-2 text-cyan-950">The Artist</div>
     <div className="lg:flex items-center text-gray-950 m-2">
-      <img src="/abhi.jpg" alt="Abhishek" className="w-36 h-36 rounded-full mr-4 " />
+      <img src="/abhi.jpeg" alt="Abhishek" className="w-36 h-36 rounded-full mr-4 " />
       <p>
           🚀 In sixth grade, I discovered Mr. Bill Gates, and in seventh grade, I encountered Mr. Mark Zuckerberg. These two individuals ignited my curiosity and sparked my interest in computer science. My journey began with a humble line of HTML code in seventh grade. As time went on, I delved into Java during eighth grade and explored C++ in eleventh grade, working on basic programs and projects along the way. Eventually, I chose Computer Science and Engineering as my career path.
           <br />
